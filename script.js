a41fd15bbf5d5a5423073f73c1ac43ff5a397398
alice-andreza <92831218+alice-andreza@users.noreply.github.com>
2021-11-14 15:26:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const ano= 2021
+let nome= prompt("Digite o seu nome: ")
+let ano_nascimento= prompt("Digite o ano de seu nascimento: ")
+let idade= 2021- ano_nascimento
+let mensagem= `Olá,seu nome é ${nome} e você tem ${idade} anos.`
+alert(mensagem)
